6cf59ef6d08bb61ef230fd6e51352219582b68b3
AngelPerez10 <111476102+AngelPerez10@users.noreply.github.com>
2025-12-30 14:53:58 -0600

    Fix database configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -116,9 +116,10 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
-if not DEBUG and os.environ.get("DATABASE_URL"):
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL:
     DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+        "default": dj_database_url.parse(DATABASE_URL)
     }
 else:
     DATABASES = {
