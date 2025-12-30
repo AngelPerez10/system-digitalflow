0f1aa649d73b03e6702486497eccc540a9241bfb
AngelPerez10 <111476102+AngelPerez10@users.noreply.github.com>
2025-12-30 15:06:35 -0600

    Add Procfile, render.json for SPA routing, and auto-create superuser

--- backend/build.sh
+++ backend/build.sh
@@ -14,3 +14,13 @@ python manage.py collectstatic --noinput
 
 # Apply database migrations
 python manage.py migrate --noinput
+
+# Crear un superusuario automáticamente
+python manage.py shell <<EOF
+from django.contrib.auth.models import User
+# Verificar si ya existe el superusuario, si no, crear uno
+if not User.objects.filter(username='AngelPerez10').exists():
+    User.objects.create_superuser('AngelPerez10', 'angeelp7457@gmail.com', 'PEREZA01FL0')
+else:
+    print("El superusuario 'AngelPerez10' ya existe.")
+EOF
